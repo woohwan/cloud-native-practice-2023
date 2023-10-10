e95b1b9ed98d53548efce71dc70d9ea220a55123
woohwan <whpark70@naver.com>
2023-10-10 22:35:55 +0900

    added ch05_02

---
+++ ch05/ch05_02/gorillamux.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func helloMuxHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello gorilla!"))
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", helloMuxHandler)
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
